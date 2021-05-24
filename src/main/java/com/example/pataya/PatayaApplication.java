60dbcdf7608cfb0dda22fafffc3be7b3c1c42376
potato <aguangzhang@yahoo.com>
2021-05-24 09:27:35 -0400

    Initial commit

---
+++ src/main/java/com/example/pataya/PatayaApplication.java
@@ -0,0 +1,13 @@
+package com.example.pataya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatayaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatayaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pataya/PatayaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pataya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatayaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
